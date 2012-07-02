c77095a3f276bf260d48def3f387c0af583f0f2b
Rogier van Apeldoorn <rogier.v.apeldoorn@gmail.com>
2012-07-02 12:54:13 +0200

    Remove Events

--- src/nl/ordina/rogier/kookschriften/client/events/GalleryUpdatedEventHandler.java
+++
@@ -1,9 +0,0 @@
-package nl.ordina.rogier.kookschriften.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface GalleryUpdatedEventHandler extends EventHandler {
-	
-	void onGalleryUpdated(GalleryUpdatedEvent event);
-
-}
